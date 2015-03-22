5d8f17daa93b92de6b97a1a7b78f5f245876be86
mono <monotom@gmail.com>
2015-03-22 20:13:53 +0100

    implement polymer demo wbsite

--- dev_root/shared/helper/shared_helper.rb
+++ dev_root/shared/helper/shared_helper.rb
@@ -2,4 +2,12 @@ module SharedHelper
   def copyright(who)
     "© #{Time.new.year} by #{who}"
   end
+
+  def select_partials(dir='*', &block)
+    folder = "#{@input_folder}/partials/#{dir}"
+    Dir.glob("#{folder}").each do |partial|
+      partial = partial.sub("#{@input_folder}/partials/", '').sub('.haml', '')
+      block.call(partial) if block_given?
+    end
+  end
 end
